567fc01c3b963e28d57b279eee1fd42823a2ebf9
Jose Joaquim <jjsn2@discente.ifpe.edu.br>
2025-11-03 21:47:37 -0300

    rota de listar enum

--- src/modulos/pedido/pedido.module.ts
+++ src/modulos/pedido/pedido.module.ts
@@ -4,15 +4,15 @@ import { Pedido } from './pedido.entity';
 import { PedidoService } from './pedido.service';
 import { PedidoController } from './pedido.controller';
 import { Pagamento } from './pagamento.entity';
-import { ClienteModule } from '../cliente/cliente.module';
 import { PedidoItem } from './pedido-item.entity';
 import { Cliente } from '../cliente/cliente.entity';
 import { Produto } from '../produto/produto.entity';
 import { Endereco } from '../endereco/endereco.entity';
+import { PagamentoController } from './pagamento.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Pedido,Pagamento,PedidoItem,Cliente,Produto,Endereco])],
-  controllers: [PedidoController],
+  controllers: [PedidoController,PagamentoController],
   providers: [PedidoService],
   exports: [PedidoService],
 })
